e77aeddf90dda1d02f3854d95c1dd9fe62c5b165
Rogges Anandarajah <roggesanandarajah@gmail.com>
2016-06-21 18:12:33 -0400

    Add files via upload

---
+++ project euler 22.py
@@ -0,0 +1,24 @@
+file=open("C:/Users/Rogges/Desktop/p022_names.txt")
+
+file1=file.read()
+
+file3=file1.split('"')
+
+alpha_values={'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H':8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z':26}
+
+file4=[x for x in file3 if x!=',' and x!='']
+
+file4.sort()
+
+score=0
+
+for name in file4:
+    index=file4.index(name)+1
+    score1=0
+    for letter in name:
+        score1+=alpha_values[letter]
+    score2=index*score1
+    score+=score2
+
+print(score)
+        
