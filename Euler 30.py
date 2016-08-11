94860fdeb1e97e6d6d053a51285ccaf84195767d
Rogges Anandarajah <roggesanandarajah@gmail.com>
2016-08-10 23:21:58 -0400

    Update and rename Euler 30 to Euler 30.py

--- Euler 30.py
+++ Euler 30.py
@@ -1,9 +1,11 @@
 power_5=[]
 
+#calculates numbers that can be expressed as the sum of their digits^5
+
 for i in range(500000):
     digits=[int(x) for x in str(i)]
     fifth_power=[x**5 for x in digits]
     if sum(fifth_power)==i:
         power_5.append(i)
 
-print(sum(power_5)-1)
+print(sum(power_5)-1) #removes the 1 that would be included by default
